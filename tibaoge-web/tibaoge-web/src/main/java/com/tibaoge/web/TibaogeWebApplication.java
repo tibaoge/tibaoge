63ae3d2c0ececd69c0d1730e365899928943e4d4
dingxk30156 <dingxk30156@hundsun.com>
2023-06-08 09:34:24 +0800

    项目基本信息

---
+++ tibaoge-web/tibaoge-web/src/main/java/com/tibaoge/web/TibaogeWebApplication.java
@@ -0,0 +1,13 @@
+package com.tibaoge.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibaogeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TibaogeWebApplication.class, args);
+    }
+
+}
---
+++ tibaoge-web/tibaoge-web/src/test/java/com/tibaoge/web/TibaogeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibaoge.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibaogeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
